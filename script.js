65a025422b2d298f6a25af6413bde62100769464
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-04-09 20:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const getData = () => {
+	return new Promise((resolve, reject)=>{
+		const textHTML = document.getElementById("text");
+		const timeHTML = document.getElementById('delay');
+		console.log()
+		setTimeout(()=>{
+			resolve(textHTML.value);
+		},timeHTML.value);
+	})
+}
+
+const btnHTML = document.getElementById("btn");
+const handleClick = async () => {
+	const text = await getData();
+	document.getElementById('output').innerText = text;
+}
+btnHTML.addEventListener('click', handleClick);
